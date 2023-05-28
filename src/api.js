1a973195d8b793ae70b2632351be20c251e6560e
Francesco Massafra <kekko4000@gmail.com>
2023-05-28 17:34:49 +0200

    Upload 0.0

--- src/api.js
+++ src/api.js
@@ -3,20 +3,28 @@ const serverless = require("serverless-http");
 
 // Create an instance of the Express app
 const app = express();
-
+const path = require('path'); 
 // Create a router to handle routes
 const router = express.Router();
 
 // Define a route that responds with a JSON object when a GET request is made to the root path
 router.get("/", (req, res) => {
   res.json({
-    hello: "hi!"
+    ciao: "hi!"
   });
 });
 
-// Use the router to handle requests to the `/.netlify/functions/api` path
-app.use(`/.netlify/functions/api`, router);
+router.get("/prova", (req, res) => {
+  res.json({
+    prova: "prova!"
+  });
+});
 
+
+// Use the router to handle requests to the `/.netlify/functions/api` path
+//app.use(`/.netlify/functions/api`, router);
+app.use('/', router); 
+app.listen(process.env.port || 3000); 
 // Export the app and the serverless function
 module.exports = app;
 module.exports.handler = serverless(app);
