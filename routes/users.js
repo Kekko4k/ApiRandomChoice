a11c79447dd6cfa52bed644c0b63e5f5a7b63721
Francesco Massafra <kekko4k@gmail.com>
2023-06-04 17:46:38 +0200

    Update

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json({prova:"123"});
 });
 
 module.exports = router;
